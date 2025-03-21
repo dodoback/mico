fccd5a57454a833c37dbaeba21a50a90b092f234
dodoback <qmelo72@naver.com>
2025-03-22 02:44:50 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+var swiper = new Swiper('.swiper-container', {
+    loop: true,
+    slidesPerView: 1, 
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    autoplay: {
+        delay: 4000,
+        disableOnInteraction: false,
+    },
+    allowTouchMove: true, // 터치 스와이프 활성화
+});
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const questions = document.querySelectorAll(".faq-question");
+
+    questions.forEach((question) => {
+        question.addEventListener("click", function () {
+            this.classList.toggle("active");
+
+            const answer = this.nextElementSibling;
+            if (answer.style.display === "block") {
+                answer.style.display = "none";
+            } else {
+                answer.style.display = "block";
+            }
+        });
+    });
+});
